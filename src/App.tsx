abd1c1d4246082972aba47d541389d36d43d8930
Gustav <gustav.jorlov@telenorconnexion.com>
2021-03-27 14:25:41 +0100

    corrected and generalized mouse event handling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { threeRenderer } from "./three/setup";
 
 const App = () => {
   const canvasEl = useRef<HTMLDivElement>(null);
-  const height = 350;
+  const height = 300;
   const width = 400;
   useEffect(() => {
     if (canvasEl && canvasEl.current) {
@@ -13,14 +13,24 @@ const App = () => {
     }
   }, []);
   return (
-    <div className="App">
-      <p>Graphics!</p>
+    <div
+      className="App"
+      style={{
+        background: "#ddd",
+        height: "100vh",
+        marginTop: "-16px",
+        paddingTop: "16px",
+      }}
+    >
+      <p style={{ color: "#000" }}>Graphics!</p>
       <div
         ref={canvasEl}
         style={{
           width: width,
           height: height,
-          border: "1px solid #ddd",
+          boxShadow: "2px 3px 10px #333333",
+          borderRadius: "4px",
+          overflow: "hidden",
           margin: "0px auto",
         }}
       ></div>
--- src/three/setup.ts
+++ src/three/setup.ts
@@ -30,53 +30,63 @@ const buildWorld = (_scene: THREE.Scene) => {
   cube1.rotation.x = 1.57 / 2;
   cube2.rotation.x = 1.57 / 3;
   cube2.rotation.y = 1.57 / 3;
-  cube2.position.x = 0.8;
+  cube2.position.x = 1.1;
   _scene.add(cube1);
   _scene.add(cube2);
 };
 
+type DispatchEventType = "click" | "hover";
 type DispatchEvent = {
-  type: string;
+  type: DispatchEventType;
   id: number | null;
 };
 
 const handleInteractions = (
   renderer: THREE.Renderer,
   scene: THREE.Scene,
-  selectedMesh: THREE.Object3D | null,
   camera: THREE.Camera,
   width: number,
   height: number,
   dispatch: (e: DispatchEvent) => void
 ): void => {
   const raycaster = new THREE.Raycaster();
-  // const onMouseMove = (e: MouseEvent) => {
-  //   mouse.x = (e.offsetX / width) * 2 - 1;
-  //   mouse.y = (e.offsetY / height) * 2 - 1;
-  // };
-  const onSelect = (e: MouseEvent) => {
-    const mousePositionInScene = (
-      width: number,
-      height: number,
-      e: MouseEvent
-    ) =>
-      new THREE.Vector2(
-        (e.offsetX / width) * 2 - 1,
-        (e.offsetY / height) * 2 - 1
-      );
-    raycaster.setFromCamera(mousePositionInScene(width, height, e), camera);
-    const intersected = raycaster.intersectObjects(scene.children);
-    console.log("onSelect", intersected);
-    if (intersected.length > 0) {
-      selectedMesh = raycaster.intersectObjects(scene.children)[0].object;
-      dispatch({ type: "click", id: selectedMesh.id });
+
+  const _mousePositionInScene = (
+    width: number,
+    height: number,
+    e: MouseEvent
+  ) =>
+    new THREE.Vector2(
+      (e.offsetX / width) * 2 - 1,
+      -1 * ((e.offsetY / height) * 2 - 1)
+    );
+
+  const _getIntersections = (e: MouseEvent) => {
+    raycaster.setFromCamera(_mousePositionInScene(width, height, e), camera);
+    return raycaster.intersectObjects(scene.children);
+  };
+  const _dispatchIfIntersected = (
+    type: DispatchEventType,
+    intersections: THREE.Intersection[]
+  ) => {
+    if (intersections.length > 0) {
+      dispatch({
+        type,
+        id: intersections[0].object.id,
+      });
     } else {
-      dispatch({ type: "click", id: null });
-      selectedMesh = null;
+      dispatch({ type, id: null });
     }
   };
-  // renderer.domElement.addEventListener("mousemove", onMouseMove, false);
+
+  const onSelect = (e: MouseEvent) => {
+    _dispatchIfIntersected("click", _getIntersections(e));
+  };
+  const onHover = (e: MouseEvent) => {
+    _dispatchIfIntersected("hover", _getIntersections(e));
+  };
   renderer.domElement.addEventListener("mousedown", onSelect, false);
+  renderer.domElement.addEventListener("mousemove", onHover, false);
 };
 
 export const threeRenderer = (
@@ -85,25 +95,22 @@ export const threeRenderer = (
 ): THREE.WebGLRenderer => {
   const { scene, camera, renderer } = setupScene(width, height);
   let selectedMesh: THREE.Object3D | null = null;
+  let hoveredMesh: THREE.Object3D | null = null;
 
   buildWorld(scene);
 
   const dispatch = (event: DispatchEvent) => {
-    console.log("dispath", event);
-    // console.log(scene.getObjectById(event.id));
-    if (event.id) selectedMesh = scene.getObjectById(event.id) || null;
-    else selectedMesh = null;
+    if (event.type === "click") {
+      if (event.id) selectedMesh = scene.getObjectById(event.id) || null;
+      else selectedMesh = null;
+    }
+    if (event.type === "hover") {
+      if (event.id) hoveredMesh = scene.getObjectById(event.id) || null;
+      else hoveredMesh = null;
+    }
   };
 
-  handleInteractions(
-    renderer,
-    scene,
-    selectedMesh,
-    camera,
-    width,
-    height,
-    dispatch
-  );
+  handleInteractions(renderer, scene, camera, width, height, dispatch);
 
   const light = new THREE.HemisphereLight(0xffffbb, 0x080820);
   scene.add(light);
@@ -112,17 +119,32 @@ export const threeRenderer = (
 
   console.log(scene);
 
-  const animate = () => {
+  const animate = (ms: number) => {
     requestAnimationFrame(animate);
-    update();
+    update(ms);
     renderer.render(scene, camera);
   };
 
-  const update = () => {
+  const update = (timeAlive: number) => {
+    // console.log("timeAlive", Math.round(timeAlive));
+    if (hoveredMesh) {
+      ((hoveredMesh as THREE.Mesh)
+        .material as THREE.MeshStandardMaterial).color.set(0x990099);
+    } else {
+      scene.children
+        .filter((c) => c.type === "Mesh")
+        .forEach((c) => {
+          ((c as THREE.Mesh).material as THREE.MeshStandardMaterial).color.set(
+            0xdddddd
+          );
+        });
+    }
     if (selectedMesh) {
-      selectedMesh.rotation.x += 0.02;
-      selectedMesh.rotation.y += 0.02;
-      selectedMesh.rotation.z += 0.02;
+      selectedMesh.position.y += 0.01 * Math.sin(timeAlive / 1000);
+      selectedMesh.position.x += 0.01 * Math.sin(timeAlive / 1000);
+      // selectedMesh.rotation.x += 0.02;
+      // selectedMesh.rotation.y += 0.02;
+      // selectedMesh.rotation.z += 0.02;
     }
     // selectedMesh?.rotation.x += 0.01;
     // selectedMesh?.rotation.y += 0.01;
@@ -137,6 +159,6 @@ export const threeRenderer = (
     // }
   };
 
-  animate();
+  animate(0);
   return renderer;
 };
